8673700bc760e0f5d63756cdff6a3b3c91d69f10
DoberBober <pochta.q@bk.ru>
2025-09-22 16:26:42 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2328159-cat-energy-2@19dab2a552864b35e7bfe0215afcbadc7f00e4d1 🚀

--- 8/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".main-header__navigation"),a=document.querySelector(".main-header__toggle");e.classList.remove("main-header__navigation--nojs");a.addEventListener("click",()=>{e.classList.toggle("main-header__navigation--closed"),e.classList.toggle("main-header__navigation--opened")});
